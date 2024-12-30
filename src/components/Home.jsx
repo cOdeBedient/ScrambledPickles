58414562e3046b542beacd575d2ff0d2d29f4216
Adam Bedient <adambedient@gmail.com>
2024-12-30 17:50:27 -0500

    Implement repair button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,6 @@
 import { useState } from 'react';
-import { pair8Players } from '../helperFunctions.jsx'
-import { pair9Players } from '../helperFunctions.jsx'
-import { useNavigate } from "react-router-dom"
 
-
-function Home({ setPairs }) {
-  const [players, setPlayers] = useState(Array(8).fill(''))
-  const [pairings, setPairings] = useState(null)
-
-  const navigate = useNavigate()
-
+function Home({ setPlayers, handleSubmit, players }) {
   const handleChange = (index, value) => {
     setPlayers((prev) => {
       const newPlayers = [...prev];
@@ -18,32 +9,17 @@ function Home({ setPairs }) {
     })
   };
 
-  // Handle form submission
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const foundPairs = pair8Players(players)
-
-    console.log("foundPairs", foundPairs)
-
-    foundPairs.forEach((round, index) => {
-      round.forEach((pair, i) => {
-        setPairs(prev => [...prev, <p className="text-sm font-bold">{pair[0]} and {pair[1]}</p>])
-      })
-    })
-
-    navigate('/pairs');
-  };
 
   return (
-    <div className="bg-green-700 h-full">
-     <h1 className="text-yellow-400 font-bold pt-6 text-3xl text-center">✣ Scrambled Pickles ✣</h1>
-      <form onSubmit={handleSubmit} className="player-form flex flex-col items-center gap-2 mt-4 bg-green-600 mx-10 py-2 rounded-tr-3xl rounded-bl-3xl">
+    <div className="bg-green-700 h-full bg-[url('pickle-icon.png')] bg-repeat bg-[size:65px_65px] pt-6">
+     <h1 className="text-black font-bold text-3xl text-center bg-yellow-400">✣ Scrambled Pickles ✣</h1>
+      <form onSubmit={handleSubmit} className="bg-opacity-95 player-form h-[85%] flex flex-col items-center justify-between mt-5 bg-green-600 mx-10 py-2 rounded-tr-3xl rounded-bl-3xl border-solid border-2 border-black">
         <h2 className="text-lg italic text-black my-2">Enter player names:</h2>
         {players.map((player, index) => (
           <div className="mb-2" key={index}>
-            <label className="rounded-tl-xl px-2 py-1 bg-green-400 border-solid border-r-0 border-2 border-black">Player {index + 1}</label>
+            <label className=" rounded-tl-xl px-2 py-1 bg-green-400 border-solid border-r-0 border-2 border-black">Player {index + 1}</label>
             <input
-              className="border-dashed border-2 border-black pl-2 bg-green-200"
+              className="h-8 border-dashed border-2 border-black pl-2 bg-green-200"
               type="text"
               value={player}
               onChange={(e) => handleChange(index, e.target.value)}
@@ -53,7 +29,9 @@ function Home({ setPairs }) {
           </div>
         ))}
         <div className="w-full flex flex-row justify-end pr-7">
-          <button className="text-black bg-yellow-400 border-solid border-2 border-black px-2 rounded-br-xl rounded-br-xl mb-3 mt-2" type="submit">Submit</button>
+          <button className="text-black px-2 w-[40%] rounded-br-xl mb-3" type="submit">
+            <img src="/paddle-button.png" alt="Logo" />
+          </button>
         </div>
       </form>
     </div>
